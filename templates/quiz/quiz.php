f9145ae6005dadfbfd32cc49384be7d97be8dc9e
Jeremy PRIEUR <jeremyprieur91@gmail.com>
2017-02-03 08:43:46 +0100

    css question

--- templates/quiz/quiz.php
+++ templates/quiz/quiz.php
@@ -10,31 +10,35 @@
                   .'</div>'; } ?>
           <h2><?php echo $quiz->getName(); ?></h2>
           <h4><?php echo $quiz->getDescription(); ?></h4>
-          <p>Une fois que vous avez commencé le quiz, vous devez répondre à toutes les questions. Si vous essayez de revenir à la question précédente, votre score sera réinitialisé et vous serez renvoyé au début.</p>
-          <p>Pour chaque question, choisissez une réponse et cliquez sur <strong>'Valider'</strong>. La question suivante vous sera alors posée.</p>
-          <p>Cet exercice a <strong><?php echo $numquestions; ?></strong> questions.</p>
+          <p>Une fois que vous avez commencé le quiz, vous devez répondre à toutes les questions. Si vous essayez de revenir à la question précédente, votre score sera réinitialisé et vous serez renvoyé au début</p>
+          <p>Pour chaque question, choisissez une réponse et cliquez sur<strong>'Soumettre une réponse'</strong>.La question suivante vous sera alors posée.</p>
+          <p>Ce quiz a <strong><?php echo $numquestions; ?></strong> questions.</p>
           <p>Vous obtiendrez votre score à la fin du quiz.</p>
           <div id="leaders-score">
                 <h4>Top <?php echo $leadersToShow; ?></h4>
                 <div class="row">
-                    <ul class="leaders col-md-6">
+                  <div class="table">
+                    <div class="table-cell">
+                      <ul class="leaders col-md-12">
                         <?php
                         $leaders = $quiz->getLeaders($leadersToShow);
                         $counter = 1;
                         foreach ($leaders as $leader) :
 
-                            echo '<li><strong>' . $leader["name"] . '</strong>: ' .  $leader["score"] . '/' . $numquestions . '</li>';
+                            echo '<li><h3>' . $leader["name"].'<strong id="score">' . $leader["score"]. '/' . $numquestions .'</strong>' .'</h3></li>';
 
                             //Use modulus to create new sub-list if required.
-                            if ($counter % (round($leadersToShow/2)) == 0) :  
-                                echo '</ul>' . PHP_EOL . '<ul class="leaders col-md-6">' . PHP_EOL;
+                            if ($counter % (round($leadersToShow/2)) == 0) :
+                                echo '</ul>' . PHP_EOL . '<ul class="leaders col-md-12">' . PHP_EOL;
                             endif;
 
                             $counter++;
 
                         endforeach;
                         ?>
-                    </ul>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
             </div><!-- leaders-score-->
               <div>
@@ -42,13 +46,13 @@
                       <p>
                           <input type="hidden" name="quizid" value="<?php echo $quiz->getId(); ?>" />
                           <input type="submit" id="submitstart" name="starter" class="btn btn-lg btn-success"
-                          value="Start Quiz" />
+                          value="Soumettre une réponse" />
                       </p>
                   </form>
               </div>
           </div>
-        
+
       </div><!-- /.row -->
-        
+
     </div><!--container-->
 <?php include 'footer.php';
--- templates/quiz/test.php
+++ templates/quiz/test.php
@@ -1,25 +1,27 @@
 <?php include 'header.php'; ?>
 <div id="container" class="quiz with-menu">
     <div class="row">
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-12">
         <?php
         if (! $session->get('last') ) :
                 $question = $quiz->getQuestion($num);
                 $answers = $quiz->getAnswers($num);
             ?>
-            <h4>Connecté en temps que : <strong><?php echo $user->getName(); ?></strong></h4>
-            <h2>Question <?php echo $num; ?>:</h2>
-            <p><?php echo $question->getText(); ?></p>
-            <form id="questionBox" method="post" action="<?php echo $root; ?>/quiz/process">
-                <ul>
-                <?php 
+
+
+            <h1 style="font-size: 50px;font-family:Roboto Bold;">Question <?php echo $num; ?></h1>
+            <p style="font-size: 25px;font-family:Roboto Bold;" class="col-md-4 col-md-offset-4"><?php echo $question->getText(); ?></p>
+
+            <form id="questionBox" class="col-md-4 col-md-offset-4" method="post" action="<?php echo $root; ?>/quiz/process">
+                <ul class="list">
+                <?php
                 $shuffledAnswers = SimpleQuiz\Utils\Base\Utils::shuffleAssoc($answers);
 
                 $acount = 0;
-                foreach ($shuffledAnswers as $answer) 
+                foreach ($shuffledAnswers as $answer)
                 {
-                    echo '<li><input type="radio" id="answer' . $acount . '" value="' . $answer . '" name="answers" />' .PHP_EOL;
-                    echo '<label for="answer' . $acount . '">' . $answer . '</label></li>' . PHP_EOL;
+                    echo '<li class="list__item"> <input type="radio" class="radio" name="foo" id="answer' . $acount . '" value="' . $answer . '" name="answers" />' .PHP_EOL;
+                    echo '<label class="label--radio" for="answer' . $acount . '">' . $answer . '</label> </li>' . PHP_EOL;
                     $acount++;
                 }
                 ?>
@@ -28,18 +30,20 @@
                 <input type="hidden" name="nonce" value="<?php echo $nonce; ?>" />
                 <input type="hidden" name="num" value="<?php echo $num; ?>" />
                 <input type="hidden" name="quizid" value="<?php echo $quiz->getId(); ?>" />
-                <input type="submit" id="submit" class="btn btn-primary" name="submit" value="Valider" />
+                <input type="submit" id="submit" class="btn btn-primary" name="submit" value="Soumetter votre réponse" />
             </p>
         </form>
+
+
         <?php else:
             $percentage = round(((int) $session->get('score') / (int) $quiz->countQuestions()) * 100);
             ?>
-            <div id="finalscore">
-            <h2 id="score"><?php echo $user->getName(); ?> a eu <?php echo $session->get('score'); ?> réponse(s) correcte(s) sur <?php echo $quiz->countQuestions(); ?></h2>
-            <h2 class="userscore"><?php echo $percentage; ?>%</h2>
-            <h3 id="time">Temps: <?php echo $timetaken['mins'] . $timetaken['secs']; ?></h3>
+            <div id="finalscore" class="col-md-6 col-md-offset-3" style="margin-top:5%;">
+            <h2 id="score" style="font-family:Roboto Bold;"><?php echo $user->getName(); ?> a repondu à <?php echo $session->get('score'); ?> sont correctes sur <?php echo $quiz->countQuestions(); ?></h2>
+            <h2 class="userscore" style="text-align:center;"><?php echo $percentage; ?>%</h2>
+            <h3 id="time" style="text-align:center;">Temps passé : <?php echo $timetaken['mins'] . $timetaken['secs']; ?></h3>
 
-            <p id="compare"><a href="<?php echo $root; ?>/quiz/<?php echo $quiz->getId(); ?>/results">Comparez votre résultat!</a></p>
+            <p id="compare" style="text-align:center;"><a class="btn btn-primary" href="<?php echo $root; ?>/quiz/<?php echo $quiz->getId(); ?>/results">Comparer vos résultats !</a></p>
             </div>
         <?php endif; ?>
         </div>
